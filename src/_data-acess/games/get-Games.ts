52355d13c342a3f318770db34529eb2d1c930f9b
Judson Ciribelli <judson.ciribelli@discente.ufma.br>
2025-07-12 23:02:43 -0300

    feat: Adding server-side function to search for games within the api

---
+++ src/_data-acess/games/get-Games.ts
@@ -0,0 +1,11 @@
+export const GetDailyGames = async () => {
+  try {
+    const res = await fetch(
+      `${process.env.NEXT_API_URL}/next-api/?api=game_day`
+    );
+    return res.json();
+  } catch (err) {
+    console.log(err);
+    throw new Error("Failed to fetch data");
+  }
+};
