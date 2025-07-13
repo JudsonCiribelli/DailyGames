7b5ca7864e6f8a4b7b9956ed5d521c662edcf178
Judson Ciribelli <judson.ciribelli@discente.ufma.br>
2025-07-12 23:13:17 -0300

    feat: adding daily games types

---
+++ src/types/game.ts
@@ -0,0 +1,9 @@
+export interface DailyGamesProps {
+  id: number;
+  title: string;
+  description: string;
+  imageUrl: string;
+  plataforms: string[];
+  categories: string[];
+  release: string;
+}
