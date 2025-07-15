8a00607e69393939227df35ea372627964e2cb97
Judson Ciribelli <judson.ciribelli@discente.ufma.br>
2025-07-14 22:55:16 -0300

    feat: ensure user can be redirect to game details page

--- src/app/_components/Gamer-Container/gameContainer.tsx
+++ src/app/_components/Gamer-Container/gameContainer.tsx
@@ -9,29 +9,26 @@ interface GameContainerProps {
 
 const GameContainer = ({ data }: GameContainerProps) => {
   return (
-    <div>
-      <h1 className="text-xl font-bold text-black text-center mt-8 mb-5">
-        Jogos exclusivos
-      </h1>
-      <Link href={`/game/${data.id}`}>
-        <section className="w-full bg-black rounded-lg">
-          <div className="w-full max-h-96 h-96 relative rounded-lg">
-            <div className="absolute z-20 bottom-0 flex items-center justify-center m-2 gap-3">
-              <p className="font-bold text-xl text-white">{data.title}</p>
-              <CircleArrowRight size={26} color="#fff" />
-            </div>
-            <Image
-              src={data.image_url}
-              alt={data.title}
-              quality={100}
-              fill={true}
-              className="max-h-86 object-cover rounded-lg opacity-50 hover:opacity-100 transition-all duration-300"
-              sizes="(max-width: 768px) 100vw, (max-width: 1200px) 44vw"
-            />
-          </div>
-        </section>
-      </Link>
-    </div>
+    <Link href={`/game/${data.id}`}>
+      <section className="w-full bg-slate-200 p-4 mb-5 rounded-lg">
+        <div className="relative w-full h-56 hover:scale-105 transition-all duration-300">
+          <Image
+            src={data.image_url}
+            alt={data.title}
+            quality={100}
+            fill={true}
+            className="max-h-86 object-cover rounded-lg opacity-50 hover:opacity-100 transition-all duration-300"
+            sizes="(max-width: 768px) 100vw, (max-width: 1200px) 44vw"
+          />
+        </div>
+        <div className="flex items-center justify-between mt-4">
+          <p className="text-sm font-bold px-2 text-black text-ellipsis truncate whitespace-normal">
+            {data.title}
+          </p>
+          <CircleArrowRight size={25} />
+        </div>
+      </section>
+    </Link>
   );
 };
 
