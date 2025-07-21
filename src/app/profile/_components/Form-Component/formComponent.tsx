389e5af3b8a994543f89214cc7e79ffb30098b67
Judson Ciribelli <judson.ciribelli@discente.ufma.br>
2025-07-20 21:44:10 -0300

    feat: add form component

---
+++ src/app/profile/_components/Form-Component/formComponent.tsx
@@ -0,0 +1,125 @@
+"use client";
+import { z } from "zod";
+import { useForm } from "react-hook-form";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+} from "@/components/ui/form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { Input } from "@/components/ui/input";
+import { useState } from "react";
+import { Button } from "@/components/ui/button";
+import { Eye, EyeOff, Loader2 } from "lucide-react";
+
+const SignSchema = z.object({
+  email: z.string().email({ message: "Email inválido" }),
+  password: z
+    .string()
+    .min(6, { message: "A senha deve ter pelo menos 6 caracteres" }),
+});
+
+type LoginSchema = z.infer<typeof SignSchema>;
+
+const FormComponent = () => {
+  const [showPassword, setShowPassword] = useState(false);
+  const [loading, setIsLoading] = useState(false);
+
+  const form = useForm<LoginSchema>({
+    resolver: zodResolver(SignSchema),
+    defaultValues: {
+      email: "",
+      password: "",
+    },
+  });
+
+  const handleLoginUser = async () => {
+    console.log(form);
+  };
+
+  return (
+    <Form {...form}>
+      <form onSubmit={handleLoginUser} className="space-y-6">
+        <FormField
+          control={form.control}
+          name="email"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel className="text-xl font-medium text-black">
+                Email
+              </FormLabel>
+              <FormControl>
+                <Input placeholder="Digite seu email" type="email" {...field} />
+              </FormControl>
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="password"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel className="text-xl font-medium text-black">
+                Senha
+              </FormLabel>
+              <FormControl>
+                <div className="relative">
+                  <Input
+                    placeholder="******"
+                    type={showPassword ? "text" : "password"}
+                    {...field}
+                    disabled={loading}
+                  />
+                  <Button
+                    type="button"
+                    variant="ghost"
+                    size="sm"
+                    className="absolute right-0 top-0 h-full px-3 py-2 hover:bg-transparent"
+                    onClick={() => setShowPassword(!showPassword)}
+                  >
+                    {showPassword ? (
+                      <EyeOff className="h-4 w-4 text-muted-foreground" />
+                    ) : (
+                      <Eye className="h-4 w-4 text-muted-foreground" />
+                    )}
+                    <span className="sr-only">
+                      {showPassword ? "Esconder senha" : "Mostrar senha"}
+                    </span>
+                  </Button>
+                </div>
+              </FormControl>
+            </FormItem>
+          )}
+        />
+        <Button
+          type="submit"
+          className="w-full bg-black text-white text-lg font-normal"
+          disabled={form.formState.isSubmitting}
+        >
+          {form.formState.isSubmitting ? (
+            <>
+              <Loader2 className="mr-2 h-4 w-4 animate-spin" />
+              Entrando...
+            </>
+          ) : (
+            "Entrar"
+          )}
+        </Button>
+        <div className="relative my-4">
+          <div className="absolute inset-0 flex items-center">
+            <span className="w-full border-t" />
+          </div>
+          <div className="relative flex justify-center text-xs uppercase">
+            <span className="bg-background px-2 text-muted-foreground">
+              Ou continue com
+            </span>
+          </div>
+        </div>
+      </form>
+    </Form>
+  );
+};
+
+export default FormComponent;
