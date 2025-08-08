82423f8964cd54cae3973a6b394a28b4af988295
Judson Ciribelli <judson.ciribelli@discente.ufma.br>
2025-08-08 13:48:52 -0300

    feat: add signUp page

--- src/app/profile/SignUp/page.tsx
+++ src/app/profile/SignUp/page.tsx
@@ -1,8 +1,34 @@
+import Link from "next/link";
+import SignUpForm from "./_components/SignUp-Form/signUpForm";
+
 const SignUpComponent = () => {
   return (
-    <div>
-      <h1>cadastre-se</h1>
-    </div>
+    <main className="w-full">
+      <div className="flex mt-40 mb-10 flex-col items-center justify-center p-4">
+        <div className="w-full max-w-md space-y-8">
+          <div className="text-center">
+            <h1 className="text-3xl font-bold">Crie sua conta</h1>
+            <p className="mt-2 text-sm text-muted-foreground">
+              Entre com suas credenciais para acessar sua conta
+            </p>
+          </div>
+
+          <SignUpForm />
+
+          <div className="text-center text-sm">
+            <p>
+              Já possui conta?{" "}
+              <Link
+                href="/profile/"
+                className="font-medium text-primary hover:underline"
+              >
+                Faça login
+              </Link>
+            </p>
+          </div>
+        </div>
+      </div>
+    </main>
   );
 };
 
